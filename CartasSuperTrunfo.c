fe64060a4fb582afb9680047d4791441c2c94acb
Adrian Oliveira <adrian.robertigarcia@gmail.com>
2026-03-20 20:11:08 +0000

    Comitando alteracoes finais. Desafio nivel mestre concluido.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -112,22 +112,26 @@ int main() {
     float super2 = (float)populacao2 + area2 + pib2 + pontos_turisticos2 + pib_p_c2 + inverso_densidade2;
 
     printf("\nComparação de Cartas:\n\n");
-    
-        /* IF (COPARATIVO) {  
-        BLOCO DE CÓDIGO A SER RODADO  ;
-        } ELSE {
-         BLOCO DE CODIGO A SER RODADO CASO IF NÃO SIGA;
-        }*/
-
 
     int menu;
-    printf("Menu para comparação:\n1- População\n2- Área\n3- PIB\n4- Número de pontos turísticos\nEscolha: ");
+    printf("\nEscolha o primeiro atributo para comparação:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("5 - Densidade Demográfica\n");
+    printf("6 - PIB per Capita\n");
+    printf("Opção: ");
     scanf("%d", &menu);
 
+    float valor1_c1, valor1_c2;
+    float valor2_c1, valor2_c2;
     switch (menu)
     {
 
     case 1:
+        valor1_c1 = populacao1;
+        valor1_c2 = populacao2;
         if (populacao1 > populacao2){
             printf("No quesito população,   %s GANHOU !\n", nome1);
             printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome1, populacao1, nome2, populacao2);
@@ -139,26 +143,32 @@ int main() {
         break;
 
     case 2:
+        valor1_c1 = area1;
+        valor1_c2 = area2;
         if (area1 > area2){
             printf("No quesito Área,   %s GANHOU !\n", nome1);
-            printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome1, area1, nome2, area2);
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, area1, nome2, area2);
         }else if (area2 > area1){
             
             printf("No quesito Área,   %s GANHOU !\n", nome2);
-            printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome2, area2, nome1, area1);
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome2, area2, nome1, area1);
         }else{printf("EMPATE NO QUESITO ÁREA!!! ");}
         break;
 
     case 3:
+        valor1_c1 = pib1;
+        valor1_c2 = pib2;
         if (pib1 > pib2){
             printf("No quesito PIB,     %s GANHOU!\n", nome1);
-            printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome1, pib1, nome2, pib2);
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, pib1, nome2, pib2);
         }else if (pib2 > pib1){
             printf("No quesito PIB,     %s GANHOU!\n", nome2);
-            printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome2, pib2, nome1, pib1);
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome2, pib2, nome1, pib1);
         }else{printf("EMPATE NO QUESITO PIB!!! ");}
         break;
     case 4:
+        valor1_c1 = pontos_turisticos1;
+        valor1_c2 = pontos_turisticos2;
         if (pontos_turisticos1 > pontos_turisticos2){
             printf("No quesito Número de pontos turísticos,     %s GANHOU!\n", nome1);
             printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome1, pontos_turisticos1, nome2, pontos_turisticos2);
@@ -168,41 +178,131 @@ int main() {
 
         }else{printf("EMPATE NO QUESITO PONTOS TURÍSTICOS!!! ");}
         break;
+    case 5:
+        valor1_c1 = densidade1;
+        valor1_c2 = densidade2;
+        if (densidade1 < densidade2){
+            printf("No quesito Densidade populacional,  CIDADE 1 GANHOU !\n");
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, inverso_densidade1, nome2, inverso_densidade2);
+        }else if (densidade2 < densidade1){
+            printf("No quesito Densidade populacional,  CIDADE 2 GANHOU !\n");
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, inverso_densidade2, nome2, inverso_densidade1);
+        }else{printf("EMPATE NO QUESITO DENSIDADE POPULACIONAL!!! ");}
+        break;
+    case 6:
+        valor1_c1 = pib_p_c1;
+        valor1_c2 = pib_p_c2;
+        if(pib_p_c1 > pib_p_c2){
+            printf("No quesito PIB per capita,  CIDADE 1 GANHOU !\n");
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, pib_p_c1, nome2, pib_p_c2);
+        }else if (pib_p_c2 > pib_p_c1){
+            printf("No quesito PIB per capita,  CIDADE 2 GANHOU !\n");
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, pib_p_c2, nome2, pib_p_c1);
+        }else{printf("EMPATE NO QUESITO PIB PER CAPITA!!! ");}
+        break;
+
     default:
         break;
     }
 
-    /*//COMPARAR POPULAÇAO
+    int menu2;
+    printf("Escolha o segundo atributo: ");
 
 
+    if(menu != 1) printf("1 - População\n");
+    if(menu != 2) printf("2 - Área\n");
+    if(menu != 3) printf("3 - PIB\n");
+    if(menu != 4) printf("4 - Pontos Turísticos\n");
+    if(menu != 5) printf("5 - Densidade Demográfica\n");
+    if(menu != 6) printf("6 - PIB per Capita\n");
+    scanf("%d", &menu2);
+    switch (menu2){
+    
 
+    case 1:
+        valor2_c1 = populacao1;
+        valor2_c2 = populacao2;
+        if (populacao1 > populacao2){
+            printf("No quesito população,   %s GANHOU !\n", nome1);
+            printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome1, populacao1, nome2, populacao2);
+        }else if (populacao2 > populacao1){
+            
+            printf("No quesito população,   %s GANHOU !\n", nome2);
+            printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome2, populacao2, nome1, populacao1);
+        }else{printf("EMPATE NO QUESITO POPULAÇAO!");}
+        break;
 
-    // COMPARAR DENSIDADE POPULACIONAL 
+    case 2:
+        valor2_c1 = area1;
+        valor2_c2 = area2;
+        if (area1 > area2){
+            printf("No quesito Área,   %s GANHOU !\n", nome1);
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, area1, nome2, area2);
+        }else if (area2 > area1){
+            
+            printf("No quesito Área,   %s GANHOU !\n", nome2);
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome2, area2, nome1, area1);
+        }else{printf("EMPATE NO QUESITO ÁREA!!! ");}
+        break;
 
-    if (densidade1 < densidade2){
-        printf("No quesito Densidade populacional,  CIDADE 1 GANHOU !\n");
-    }else{
-        printf("No quesito Densidade populacional,  CIDADE 2 GANHOU !\n");
+    case 3:
+        valor2_c1 = pib1;
+        valor2_c2 = pib2;
+        if (pib1 > pib2){
+            printf("No quesito PIB,     %s GANHOU!\n", nome1);
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, pib1, nome2, pib2);
+        }else if (pib2 > pib1){
+            printf("No quesito PIB,     %s GANHOU!\n", nome2);
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome2, pib2, nome1, pib1);
+        }else{printf("EMPATE NO QUESITO PIB!!! ");}
+        break;
+    case 4:
+        valor2_c1 = pontos_turisticos1;
+        valor2_c2 = pontos_turisticos2;
+        if (pontos_turisticos1 > pontos_turisticos2){
+            printf("No quesito Número de pontos turísticos,     %s GANHOU!\n", nome1);
+            printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome1, pontos_turisticos1, nome2, pontos_turisticos2);
+        }else if (pontos_turisticos2 > pontos_turisticos1){
+            printf("No quesito Número de pontos turísticos,     %s GANHOU!\n", nome2);
+            printf("%s tem: %d, \n enquanto...\n %s tem: %d", nome2, pontos_turisticos2, nome1, pontos_turisticos1);
+
+        }else{printf("EMPATE NO QUESITO PONTOS TURÍSTICOS!!! ");}
+        break;
+    case 5:
+        valor2_c1 = densidade1;
+        valor2_c2 = densidade2;
+        if (densidade1 < densidade2){
+            printf("No quesito Densidade populacional,  CIDADE 1 GANHOU !\n");
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, inverso_densidade1, nome2, inverso_densidade2);
+        }else if (densidade2 < densidade1){
+            printf("No quesito Densidade populacional,  CIDADE 2 GANHOU !\n");
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, inverso_densidade2, nome2, inverso_densidade1);
+        }else{printf("EMPATE NO QUESITO DENSIDADE POPULACIONAL!!! ");}
+        break;
+    case 6:
+        valor2_c1 = pib_p_c1;
+        valor2_c2 = pib_p_c2;
+        if(pib_p_c1 > pib_p_c2){
+            printf("No quesito PIB per capita,  CIDADE 1 GANHOU !\n");
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, pib_p_c1, nome2, pib_p_c2);
+        }else if (pib_p_c2 > pib_p_c1){
+            printf("No quesito PIB per capita,  CIDADE 2 GANHOU !\n");
+            printf("%s tem: %.2f, \n enquanto...\n %s tem: %.2f", nome1, pib_p_c2, nome2, pib_p_c1);
+        }else{printf("EMPATE NO QUESITO PIB PER CAPITA!!! ");}
+        break;
+
+    default:
+        break;
     }
+    float soma1 = valor1_c1 + valor2_c1;
+    float soma2 = valor1_c2 + valor2_c2;
 
+    printf("\nResultado final:\n");
+    (soma1 > soma2) ? printf("%s venceu!\n", nome1) :
+    (soma2 > soma1) ? printf("%s venceu!\n", nome2) :
+    printf("Empate!\n");
+    
 
-    //COMPARAR SUPER
-    if (super1 > super2){
-        printf("No quesito SUPER,   Cidade 1 ganhou !\n");
-    }
-    else {
-        printf("No quesito SUPER,   Cidade 2 ganhou !\n");
-    }
-/*
-    printf("População: Carta 1 venceu (%d)\n", populacao1 > populacao2);
-    printf("Área: Carta 1 venceu (%d)\n", area1 > area2);
-    printf("PIB: Carta 1 venceu (%d)\n", pib1 > pib2);
-    printf("Pontos Turísticos: Carta 1 venceu (%d)\n", pontos_turisticos1 > pontos_turisticos2);
-
-    printf("Densidade Populacional: Carta 1 venceu (%d)\n", densidade1 < densidade2);
-
-    printf("PIB per Capita: Carta 1 venceu (%d)\n", pib_p_c1 > pib_p_c2);
-    printf("Super Poder: Carta 1 venceu (%d)\n", super1 > super2);*/
-
+    
     return 0;
 }
